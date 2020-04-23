a5bd2ba7b29558fe418f7aea5537fb205b782635
William Bellavnce <flintman@flintmancomputers.com>
2020-04-23 06:20:51 -0400

    move fsab to vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,7 +29,7 @@ LOCAL_MODULE       := fstab.tenderloin
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.tenderloin
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
